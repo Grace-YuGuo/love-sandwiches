3f3142d3580a52d7b48a02dd1fcd1cec47b7fcdb
Yu Guo <grace.guo012@gmail.com>
2024-07-15 11:31:33 +0000

    Feat:create the get_sales_data function to get data string from user

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# set scope lists the APIs that the program should access in order to run. scope is not changeable that constant variable as SCOPE
+# set scope lists the APIs that the program should access in order to run. scope is not changeable so to define SCOPE as constant variable
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -17,9 +17,21 @@ GSPRED_CLIENT=gspread.authorize(SCOPED_CREDS)
 SHEET=GSPRED_CLIENT.open('love_sandwiches')
 
 # create variable for sheet data values
-sales = SHEET.worksheet('sales')
-data =sales.get_all_values()
-print(data)
+# sales = SHEET.worksheet('sales')
+# data =sales.get_all_values()
 
+# define the get_user_data function
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print('Please enter sales data from the last market.')
+    print('Data should be six numbers, separated by commas.')
+    print('Example: 10,20,30,40,50,60\n')
+
+    data_str=input('Enter your data here:')
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
 
 
