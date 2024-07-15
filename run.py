61d9260f3f2b53b4e56cc9388078b58f5770fcf6
Yu Guo <grace.guo012@gmail.com>
2024-07-15 13:55:55 +0000

    Feat: add while true loop to repeat input request until user input validation is passed

--- run.py
+++ run.py
@@ -25,18 +25,21 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print('Please enter sales data from the last market.')
-    print('Data should be six numbers, separated by commas.')
-    print('Example: 10,20,30,40,50,60\n')
-
-    data_str=input('Enter your data here:')
-    sales_data= data_str.split(',')
-    validate_data(sales_data)
+    while True:
+        print('Please enter sales data from the last market.')
+        print('Data should be six numbers, separated by commas.')
+        print('Example: 10,20,30,40,50,60\n')
 
 
-    # print(f"sales_data is {sales_data}")
-    # print(f"{data_str}")
-    # print(f"{sales}")
+        data_str=input('Enter your data here:')
+        sales_data= data_str.split(',')
+        
+
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+
+    return sales_data
 
 # define validate_sales_data function
 def validate_data(values):
@@ -47,15 +50,18 @@ def validate_data(values):
     """
     try:
         [int(item) for item in values]
-        if len(sales)!=6:
+        if len(values)!=6:
             raise ValueError(
                 f'Exactly 6 values required, you provided {len(values)}')
-        print(sales)
+        
     except ValueError as e:
         print(f"Invalid data: {e},please try again.\n")
+        return False
+
+    return True
     
 
 
-get_sales_data()
+data=get_sales_data()
 
 
